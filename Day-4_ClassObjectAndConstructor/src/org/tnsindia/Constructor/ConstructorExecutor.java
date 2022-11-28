4b955ba0c542a39c97c22df06024fc733cd8a4d9
Esrabanu <esrabanu7786@gmail.com>
2022-11-28 16:57:15 +0530

    first commit

---
+++ Day-4_ClassObjectAndConstructor/src/org/tnsindia/Constructor/ConstructorExecutor.java
@@ -0,0 +1,19 @@
+package org.tnsindia.Constructor;
+
+public class ConstructorExecutor {
+
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		Employee e1=new Employee(25000,"Nikita");
+		Employee e2=new Employee(45000,"Chaitali");
+
+		e1.print();
+		e2.print();
+
+	}
+
+
+
+	}
+
+
---
+++ Day-4_ClassObjectAndConstructor/src/org/tnsindia/classdemo/Car.java
@@ -0,0 +1,27 @@
+package org.tnsindia.classdemo;
+
+public class Car {
+
+	public static void main(String[] args) {
+	}
+	
+		Car()
+		{
+			System.out.println("Default Constructor");
+		}
+		
+		
+		//data members
+		int speed;
+		void speed(int speed)
+		{
+			System.out.println("Speed of the car is: "+speed);
+		
+
+	}
+	
+
+
+	}
+
+
