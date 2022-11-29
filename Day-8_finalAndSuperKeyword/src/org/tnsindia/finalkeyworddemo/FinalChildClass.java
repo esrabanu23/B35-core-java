ba1f11af83d4a61399801ab60e9876b8ac6ef940
Esrabanu <esrabanu7786@gmail.com>
2022-11-29 20:56:09 +0530

    first commit

---
+++ Day-8_finalAndSuperKeyword/src/org/tnsindia/finalkeyworddemo/FinalChildClass.java
@@ -0,0 +1,15 @@
+package org.tnsindia.finalkeyworddemo;
+//child class
+public class FinalChildClass extends FinalParentClass{
+	
+	//method
+	public void print()
+	{
+		System.out.println(name);
+	}
+	
+	
+	
+
+}
+
---
+++ Day-8_finalAndSuperKeyword/src/org/tnsindia/finalkeyworddemo/FinalMethodWithParent.java
@@ -0,0 +1,18 @@
+package org.tnsindia.finalkeyworddemo;
+
+//parent class
+public class FinalMethodWithParent {
+	
+	public long aadhar_no;
+	
+	//final keyword with method
+	public final void print()
+	{
+		System.out.println("Aadhar no.is: "+aadhar_no);
+	}
+	
+
+
+
+
+}
