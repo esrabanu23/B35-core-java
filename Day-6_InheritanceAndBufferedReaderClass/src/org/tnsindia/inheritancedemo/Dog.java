8486ed16d79636197c341b6c6d1ca9f86fe7c973
Esrabanu <esrabanu7786@gmail.com>
2022-11-29 21:01:12 +0530

    first commit

--- Day-6_InheritanceAndBufferedReaderClass/src/org/tnsindia/inheritancedemo/Dog.java
+++ Day-6_InheritanceAndBufferedReaderClass/src/org/tnsindia/inheritancedemo/Dog.java
@@ -2,10 +2,15 @@ package org.tnsindia.inheritancedemo;
 
 public class Dog {
 	public String breed;
+	public String name;
 	public void print()
 	{
 		System.out.println("The breed of the dog is: "+breed);
 
+	}
+	public void display() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
