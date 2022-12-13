820ce43b2db2792d6a8d5c70f1b124a948cdce31
Esrabanu <esrabanu7786@gmail.com>
2022-12-13 16:56:42 +0530

    first commit

---
+++ Day-13_ExceptionHandling/src/org/tnsindia/customexceptiondemo/LoginCredentials.java
@@ -0,0 +1,20 @@
+package org.tnsindia.customexceptiondemo;
+
+//Custom Exception for validating login
+public class LoginCredentials extends Exception{
+	
+	private String id;
+	private String password;
+
+	
+
+	public LoginCredentials(String id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+}
+
+
+	
+	
