6fc094c8b4478c4c99e52edfab5ff1a80ee13df0
Esrabanu <esrabanu7786@gmail.com>
2022-12-17 18:53:07 +0530

    first commit

---
+++ Day-17_JavaAnnotations/src/org/tnsindia/builtinannotationdemo/Child.java
@@ -0,0 +1,13 @@
+package org.tnsindia.builtinannotationdemo;
+
+public class Child extends parent{
+	public String name="Diya";
+	//Indicates that a method declaration is intended to override amethod declaration in a supertype. 
+	@Override
+	public void display()
+	{
+		super.display();
+		System.out.println("Child Name: "+name);
+	}	
+	
+}
